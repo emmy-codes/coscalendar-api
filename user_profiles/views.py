3ca292fca13013f394f12f705b4aaf964985636a
Emma <alunia.moon@gmail.com>
2024-05-25 12:07:46 +0200

    Add login/logout views, IsCosplayer permissions and is_cosplayer field

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -4,34 +4,43 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import UserProfile
 from .serializers import UserProfileSerializer
+from coscalendar_api.permissions import IsCosplayerOrReadOnly
 
 
 class UserProfileList(APIView):
     def get(self, request):
         user_profiles = UserProfile.objects.all()
-        serializer = UserProfileSerializer(user_profiles, many=True)
+        serializer = UserProfileSerializer(
+            user_profiles, many=True, context={"request": request}
+        )
         return Response(serializer.data)
 
 
 class UserProfileDetail(APIView):
-    
+    permission_classes = [IsCosplayerOrReadOnly]
     serializer_class = UserProfileSerializer
     
     def get_object(self, pk):
         try:
-            profile = UserProfile.objects.get(pk=pk)
-            return profile
+            user_profile = UserProfile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, user_profile)
+            return user_profile
         except UserProfile.DoesNotExist:
             raise Http404
     
     def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = UserProfileSerializer(profile)
+        user_profile = self.get_object(pk)
+        serializer = UserProfileSerializer(
+            user_profile, context={"request": request}
+        )
         return Response(serializer.data)
 
     def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = UserProfileSerializer(profile, data=request.data)
+        user_profile = self.get_object(pk)
+        serializer = UserProfileSerializer(
+            user_profile, data=request.data, context={"request": request}
+        )
+        
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
