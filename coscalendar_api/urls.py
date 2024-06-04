01697b1558f2d8fdbb11fd9c4398b2a7cf64e9d5
Emma <alunia.moon@gmail.com>
2024-06-04 16:16:08 +0200

    reshuffled order of urlpatterns to hopefully fix post request issue from frontend

--- coscalendar_api/urls.py
+++ coscalendar_api/urls.py
@@ -5,18 +5,17 @@ from user_profiles import views as user_profiles_views
 from .views import root_route, logout_route
 
 urlpatterns = [
-    path("", root_route),
     path("admin/", admin.site.urls),
     path("api-auth/", include(
         "rest_framework.urls", namespace="rest_framework"
         )
     ),
-    path("dj-rest-auth/logout", logout_route),
     path("dj-rest-auth/", include("dj_rest_auth.urls")),
     path("dj-rest-auth/registration/", include(
-        "dj-rest-auth.registration.urls"
+        "dj_rest_auth.registration.urls"
         )
     ),
+    path("dj-rest-auth/logout", logout_route),
     # Include user_profile URLs directly
     path("user_profiles/", user_profiles_views.UserProfileList.as_view()),
     path(
@@ -25,4 +24,5 @@ urlpatterns = [
     ),
     path("", include("cosplans.urls")),
     path("", include("cosexpenses.urls")),
+    path("", root_route),
 ]
