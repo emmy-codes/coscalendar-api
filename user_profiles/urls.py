6346d13cc53bbcd2d6c06e1b2a7550413fda4724
Emma <alunia.moon@gmail.com>
2024-06-21 16:24:24 +0200

    Update UserProfile to receive the current logged in user data

--- user_profiles/urls.py
+++ user_profiles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from user_profiles import views
 
 urlpatterns = [
-    path("user_profiles/", views.UserProfileList.as_view()),
-    path("user_profiles/<int:pk>/", views.UserProfileDetail.as_view())
+    # path("user_profiles/", views.UserProfileCreateOrUpdateView.as_view()),
+    path("user_profiles/", views.UserProfileDetail.as_view())
 ]
--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, generics, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import UserProfile
@@ -17,26 +17,26 @@ class UserProfileList(APIView):
 
 
 class UserProfileDetail(APIView):
-    permission_classes = [IsCosplayerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = UserProfileSerializer
 
-    def get_object(self, pk):
+    def get_object(self):
         try:
-            user_profile = UserProfile.objects.get(pk=pk)
+            user_profile = UserProfile.objects.get(cosplayer=self.request.user)
             self.check_object_permissions(self.request, user_profile)
             return user_profile
         except UserProfile.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk):
-        user_profile = self.get_object(pk)
+    def get(self, request):
+        user_profile = UserProfile.objects.get(cosplayer=self.request.user)
         serializer = UserProfileSerializer(
             user_profile, context={"request": request}
         )
         return Response(serializer.data)
 
-    def put(self, request, pk):
-        user_profile = self.get_object(pk)
+    def put(self, request, *args, **kwargs):
+        user_profile = UserProfile.objects.get(cosplayer=self.request.user)
         serializer = UserProfileSerializer(
             user_profile, data=request.data, context={"request": request}
         )
@@ -45,3 +45,20 @@ class UserProfileDetail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserProfileCreateOrUpdateView(generics.CreateAPIView, generics.UpdateAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def put(self, serializer):
+        # connects the new UserProfile with the currently logged-in user
+        serializer.save(cosplayer=self.request.user) 
+
+    def get_object(self):
+        # get the UserProfile object for the currently logged-in user
+        try:
+            return UserProfile.objects.get(cosplayer=self.request.user)
+        except UserProfile.DoesNotExist:
+            raise Http404
