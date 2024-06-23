f61f737ab007a8c666363636079a848452544cde
Emma <alunia.moon@gmail.com>
2024-06-23 16:21:22 +0200

    Attempting to fix issue where all cosplans were showing to all users by querying the cosplayer

--- cosplans/views.py
+++ cosplans/views.py
@@ -7,6 +7,7 @@ from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import request
+from django.db.models import Q
 
 
 class CosPlanList(ListCreateAPIView):
@@ -38,7 +39,7 @@ class CosPlanList(ListCreateAPIView):
 
 class CosPlanListByDateRange(ListCreateAPIView):
     def get_queryset(self):
-        return CosPlan.objects.filter(due_date__range=[self.request.query_params.get("startDate"), self.request.query_params.get("endDate")]).order_by("due_date")
+        return CosPlan.objects.filter(Q(cosplayer=self.request.user), due_date__range=[self.request.query_params.get("startDate"), self.request.query_params.get("endDate")]).order_by("due_date")
     serializer_class = CosPlanSerializer
     permission_classes = [IsCosplayerOrReadOnly]
 
