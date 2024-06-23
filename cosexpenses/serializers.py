3902b73fef37010e690e68477163ed3137f26859
Emma <alunia.moon@gmail.com>
2024-06-23 13:48:40 +0200

    remove reference of cosplay_name to try and solve deployment issue

--- cosexpenses/serializers.py
+++ cosexpenses/serializers.py
@@ -5,14 +5,12 @@ from .models import CosExpense
 class CosExpenseSerializer(serializers.ModelSerializer):
     # will show cosplay name instead of the id
     cosplayer = serializers.ReadOnlyField(source="cosplayer.username")
-    cosplay_name = serializers.ReadOnlyField(source="cosplan.cosplay")
 
     class Meta:
         model = CosExpense
         fields = [
             "id",
             "cosplayer",
-            "cosplay_name",
             "item_name",
             "quantity",
             "unit_price",
